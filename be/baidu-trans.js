f6527b2aafb7e6a025616e6a2606f49f812a17eb
445218339@qq.com <yuanhaokai@meituan.com>
2024-10-11 10:39:25 +0800

    feat:提升代码可读性

--- be/baidu-trans.js
+++ be/baidu-trans.js
@@ -1,54 +1,132 @@
-const a = (t) =>{
-    var o, i = t.match(/[\uD800-\uDBFF][\uDC00-\uDFFF]/g);
-    if (null === i) {
-        var a = t.length;
-        a > 30 && (t = "".concat(t.substr(0, 10)).concat(t.substr(Math.floor(a / 2) - 5, 10)).concat(t.substr(-10, 10)))
-    } else {
-        for (var s = t.split(/[\uD800-\uDBFF][\uDC00-\uDFFF]/), c = 0, u = s.length, l = []; c < u; c++)
-            "" !== s[c] && l.push.apply(l, function(t) {
-                if (Array.isArray(t))
-                    return e(t)
-            }(o = s[c].split("")) || function(t) {
-                if ("undefined" != typeof Symbol && null != t[Symbol.iterator] || null != t["@@iterator"])
-                    return Array.from(t)
-            }(o) || function(t, n) {
-                if (t) {
-                    if ("string" == typeof t)
-                        return e(t, n);
-                    var r = Object.prototype.toString.call(t).slice(8, -1);
-                    return "Object" === r && t.constructor && (r = t.constructor.name),
-                    "Map" === r || "Set" === r ? Array.from(t) : "Arguments" === r || /^(?:Ui|I)nt(?:8|16|32)(?:Clamped)?Array$/.test(r) ? e(t, n) : void 0
-                }
-            }(o) || function() {
-                throw new TypeError("Invalid attempt to spread non-iterable instance.\nIn order to be iterable, non-array objects must have a [Symbol.iterator]() method.")
-            }()),
-            c !== u - 1 && l.push(i[c]);
-        var p = l.length;
-        p > 30 && (t = l.slice(0, 10).join("") + l.slice(Math.floor(p / 2) - 5, Math.floor(p / 2) + 5).join("") + l.slice(-10).join(""))
+/**
+ * 主函数，处理输入并返回结果
+ */
+const processInput = ({ input }) => {
+    const processedString = truncateString({ input });
+    const encodedData = encodeUTF8({ str: processedString });
+    const result = calculate({ encodedData, key: r });
+    return result;
+  };
+  
+  /**
+   * 处理并可能截断输入字符串
+   */
+  const truncateString = ({ input, maxLength = 30 }) => {
+    const unicodeChars = input.match(/[\uD800-\uDBFF][\uDC00-\uDFFF]/g);
+    
+    if (!unicodeChars) {
+      return truncateNonUnicodeString({ input, maxLength });
     }
-    for (var d = "".concat(String.fromCharCode(103)).concat(String.fromCharCode(116)).concat(String.fromCharCode(107)), h = (null !== r ? r : (r = window[d] || "") || "").split("."), f = Number(h[0]) || 0, m = Number(h[1]) || 0, g = [], y = 0, v = 0; v < t.length; v++) {
-        var _ = t.charCodeAt(v);
-        _ < 128 ? g[y++] = _ : (_ < 2048 ? g[y++] = _ >> 6 | 192 : (55296 == (64512 & _) && v + 1 < t.length && 56320 == (64512 & t.charCodeAt(v + 1)) ? (_ = 65536 + ((1023 & _) << 10) + (1023 & t.charCodeAt(++v)),
-        g[y++] = _ >> 18 | 240,
-        g[y++] = _ >> 12 & 63 | 128) : g[y++] = _ >> 12 | 224,
-        g[y++] = _ >> 6 & 63 | 128),
-        g[y++] = 63 & _ | 128)
+    
+    return truncateUnicodeString({ input, unicodeChars, maxLength });
+  };
+  
+  /**
+   * 截断非 Unicode 字符串
+   */
+  const truncateNonUnicodeString = ({ input, maxLength }) => {
+    if (input.length <= maxLength) return input;
+    
+    const start = input.substr(0, 10);
+    const middle = input.substr(Math.floor(input.length / 2) - 5, 10);
+    const end = input.substr(-10, 10);
+    
+    return `${start}${middle}${end}`;
+  };
+  
+  /**
+   * 截断包含 Unicode 字符的字符串
+   */
+  const truncateUnicodeString = ({ input, unicodeChars, maxLength }) => {
+    const parts = input.split(/[\uD800-\uDBFF][\uDC00-\uDFFF]/);
+    const chars = parts.flatMap((part, index) => [
+      ...part.split(''),
+      ...(index < unicodeChars.length ? [unicodeChars[index]] : [])
+    ]);
+  
+    if (chars.length <= maxLength) return chars.join('');
+  
+    const start = chars.slice(0, 10).join('');
+    const middle = chars.slice(Math.floor(chars.length / 2) - 5, Math.floor(chars.length / 2) + 5).join('');
+    const end = chars.slice(-10).join('');
+  
+    return `${start}${middle}${end}`;
+  };
+  
+  /**
+   * 将字符串编码为 UTF-8
+   */
+  const encodeUTF8 = ({ str }) => {
+    const result = [];
+    
+    for (let i = 0; i < str.length; i++) {
+      const code = str.charCodeAt(i);
+      
+      if (code < 128) {
+        result.push(code);
+      } else if (code < 2048) {
+        result.push((code >> 6) | 192);
+        result.push((code & 63) | 128);
+      } else if (
+        (code & 0xFC00) === 0xD800 &&
+        i + 1 < str.length &&
+        (str.charCodeAt(i + 1) & 0xFC00) === 0xDC00
+      ) {
+        const surrogate = 0x10000 + ((code & 0x3FF) << 10) + (str.charCodeAt(++i) & 0x3FF);
+        result.push((surrogate >> 18) | 240);
+        result.push(((surrogate >> 12) & 63) | 128);
+        result.push(((surrogate >> 6) & 63) | 128);
+        result.push((surrogate & 63) | 128);
+      } else {
+        result.push((code >> 12) | 224);
+        result.push(((code >> 6) & 63) | 128);
+        result.push((code & 63) | 128);
+      }
     }
-    for (var b = f, w = "".concat(String.fromCharCode(43)).concat(String.fromCharCode(45)).concat(String.fromCharCode(97)) + "".concat(String.fromCharCode(94)).concat(String.fromCharCode(43)).concat(String.fromCharCode(54)), k = "".concat(String.fromCharCode(43)).concat(String.fromCharCode(45)).concat(String.fromCharCode(51)) + "".concat(String.fromCharCode(94)).concat(String.fromCharCode(43)).concat(String.fromCharCode(98)) + "".concat(String.fromCharCode(43)).concat(String.fromCharCode(45)).concat(String.fromCharCode(102)), x = 0; x < g.length; x++)
-        b = n(b += g[x], w);
-    return b = n(b, k),
-    (b ^= m) < 0 && (b = 2147483648 + (2147483647 & b)),
-    "".concat((b %= 1e6).toString(), ".").concat(b ^ f)
-}
-
-function n(t, e) {
-    for (var n = 0; n < e.length - 2; n += 3) {
-        var r = e.charAt(n + 2);
-        r = "a" <= r ? r.charCodeAt(0) - 87 : Number(r),
-        r = "+" === e.charAt(n + 1) ? t >>> r : t << r,
-        t = "+" === e.charAt(n) ? t + r & 4294967295 : t ^ r
+    
+    return result;
+  };
+  
+  /**
+   * 使用编码后的数据进行计算
+   */
+  const calculate = ({ encodedData, key }) => {
+    const [f, m] = key.split('.').map(Number);
+    let b = f;
+  
+    const w = "+-a" + "^+6";
+    const k = "+-3" + "^+b" + "+-f";
+  
+    for (const value of encodedData) {
+      b = n(b + value, w);
     }
-    return t
-}
-const r = "320305.131321201";
-console.log(a('拉拉1'))
+  
+    b = n(b, k);
+    b ^= m;
+  
+    if (b < 0) {
+      b = 2147483648 + (2147483647 & b);
+    }
+  
+    b %= 1e6;
+  
+    return `${b.toString()}.${b ^ f}`;
+  };
+  
+  /**
+   * 辅助计算函数
+   */
+  const n = (t, e) => {
+    for (let n = 0; n < e.length - 2; n += 3) {
+      let r = e.charAt(n + 2);
+      r = 'a' <= r ? r.charCodeAt(0) - 87 : Number(r);
+      r = '+' === e.charAt(n + 1) ? t >>> r : t << r;
+      t = '+' === e.charAt(n) ? (t + r) & 4294967295 : t ^ r;
+    }
+    return t;
+  };
+  
+  // 使用示例
+  const r = "320305.131321201";
+  console.log(processInput({ input: 'haha' }));
+  
